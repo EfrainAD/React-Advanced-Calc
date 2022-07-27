de7f85fc6c17f4fce72c6549c08dd9c195935b22
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-07-26 21:34:19 -0400

    Forked, npm i, set up the buttons with onclick that returns the button value.

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,42 +2,48 @@ import React, { Component } from 'react'
 
 class Calculator extends Component {
     // Declare state variables
-
+    state = {
+        answer: 0,
+        input: null,
+        Operator: null // '+', '-', '/', '*', or null
+    }
+    print = () => {console.log('hello')}
+    handleClick = (e) => console.log(e)
 render(){
     return (
         <div className="container">
             <h1>React Calculator</h1>
             <div className="calc-container">
-                <p>Values: </p>
-                <div className="answer-box">TBD</div>
+                <p onClick={this.print}>Values: </p>
+                <div className="answer-box">{this.state.answer}</div>
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
-                    <button className="calc-button calc-button-top">+/-</button>
-                    <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button className="calc-button calc-button-top" onClick={()=>this.handleClick("AC")}>AC</button>
+                    <button className="calc-button calc-button-top" onClick={()=>this.handleClick("+/-")}>+/-</button>
+                    <button className="calc-button calc-button-top" onClick={()=>this.handleClick("%")}>%</button>
+                    <button className="calc-button calc-button-op" onClick={()=>this.handleClick("/")}>/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                    <button className="calc-button" onClick={()=>this.handleClick("7")} >7</button>
+                    <button className="calc-button" onClick={()=>this.handleClick("8")}>8</button>
+                    <button className="calc-button" onClick={()=>this.handleClick("9")}>9</button>
+                    <button className="calc-button calc-button-op" onClick={()=>this.handleClick("x")}>x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                    <button className="calc-button" onClick={()=>this.handleClick("4")}>4</button>
+                    <button className="calc-button" onClick={()=>this.handleClick("5")}>5</button>
+                    <button className="calc-button" onClick={()=>this.handleClick("6")}>6</button>
+                    <button className="calc-button calc-button-op" onClick={()=>this.handleClick("-")}>-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
-                    <button className="calc-button calc-button-op">+</button>
+                    <button className="calc-button" onClick={()=>this.handleClick("1")}>1</button>
+                    <button className="calc-button" onClick={()=>this.handleClick("2")}>2</button>
+                    <button className="calc-button" onClick={()=>this.handleClick("3")}>3</button>
+                    <button className="calc-button calc-button-op" onClick={()=>this.handleClick("+")}>+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
-                    <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op">=</button>
+                    <button className="calc-button width-2" onClick={()=>this.handleClick("0")}>0</button>
+                    <button className="calc-button" onClick={()=>this.handleClick(".")}>.</button>
+                    <button className="calc-button calc-button-op" onClick={()=>this.handleClick("=")}>=</button>
                 </div>
             </div>
         </div>
